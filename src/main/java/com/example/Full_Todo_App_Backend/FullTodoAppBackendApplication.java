5132c6ac9a66c69df5b537c53a80e501ceb6baf8
Peder Goodman <peder.goodman@gmail.com>
2024-11-25 12:24:32 -0600

    initial commit

---
+++ src/main/java/com/example/Full_Todo_App_Backend/FullTodoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Full_Todo_App_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullTodoAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullTodoAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Full_Todo_App_Backend/FullTodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Full_Todo_App_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTodoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
