49027d836c117bafca0e7face4a66d89685e51c9
Peder Goodman <peder.goodman@gmail.com>
2024-11-26 16:47:05 -0600

    update user model

--- src/main/java/com/example/Full_Todo_App_Backend/model/User.java
+++ src/main/java/com/example/Full_Todo_App_Backend/model/User.java
@@ -1,12 +1,11 @@
 package com.example.Full_Todo_App_Backend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
@@ -17,6 +16,22 @@ public class User {
 
     @Id
     private String id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String email;
+    private String oktaId;
+
+    // lombok provides all args and no args constructor
+    // this is for everything but the id (and will probably be used most often
+    public User(String firstName, String lastName, String email, String oktaId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.oktaId = oktaId;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+
+    }
 }
