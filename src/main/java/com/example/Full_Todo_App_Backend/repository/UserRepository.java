26c44e53f79b546123502df033cad1b7ea2c8bfa
Peder Goodman <peder.goodman@gmail.com>
2024-11-27 16:33:25 -0600

    chang userRepository type

--- src/main/java/com/example/Full_Todo_App_Backend/repository/UserRepository.java
+++ src/main/java/com/example/Full_Todo_App_Backend/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.Full_Todo_App_Backend.repository;
 
-import com.example.Full_Todo_App_Backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Full_Todo_App_Backend.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByOktaId(String oktaId);
 }
 
