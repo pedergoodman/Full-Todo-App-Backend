9df6807b501b3dd3f4864ee3272e48c34e4f8246
Peder Goodman <peder.goodman@gmail.com>
2024-11-27 12:22:20 -0600

    add ability to save user to DB if they don't exist in the DB

--- src/main/java/com/example/Full_Todo_App_Backend/repository/UserRepository.java
+++ src/main/java/com/example/Full_Todo_App_Backend/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.Full_Todo_App_Backend.repository;
 import com.example.Full_Todo_App_Backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findUserByOktaId(String oktaId);
 }
 
--- src/main/java/com/example/Full_Todo_App_Backend/service/UserService.java
+++ src/main/java/com/example/Full_Todo_App_Backend/service/UserService.java
@@ -1,9 +1,11 @@
 package com.example.Full_Todo_App_Backend.service;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.Full_Todo_App_Backend.model.User;
 import com.example.Full_Todo_App_Backend.repository.UserRepository;
@@ -16,7 +18,8 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User checkAndSaveAuthenticatedUser(OAuth2User user) {
+    @Transactional
+    public synchronized User checkAndSaveAuthenticatedUser(OAuth2User user) {
         // Deconstruct user object from Okta and make it usable for our DB needs
         Map<String, Object> attributes = user.getAttributes();
         String email = (String) attributes.get("preferred_username");
@@ -27,17 +30,18 @@ public class UserService {
         // create new user object
         User currentUser = new User(first_name, last_name, email, oktaId);
 
-        System.out.println("User object: " + currentUser);
-        // System.out.println("User fullname: " + currentUser.getFullName());
-
         // check if user exists in database
+        Optional<User> doesOktaUserExist = userRepository.findUserByOktaId(oktaId);
 
         // if user exists, send user data back to database
-        // if user doesn't exists, add them to DB, then send user data back to database
-
-        // userService.checkAndSaveAuthenticatedUser(currentUser);
-
-        // this is where we would connect it to the DB!
-        return currentUser;
-    };
+        if (doesOktaUserExist.isPresent()) {
+            return doesOktaUserExist.get();
+        } else {
+            // if user doesn't exist, add them to DB, then send user data to frontend from
+            // database
+            userRepository.save(currentUser);
+            return userRepository.findUserByOktaId(oktaId)
+                    .orElseThrow(() -> new IllegalStateException("Okta User " + oktaId + " did not save"));
+        }
+    }
 }
